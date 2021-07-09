f30da46824fb072ae5c9098cfee0abab3606f44e
mustafa <ucar.mustafa@outlook.com.tr>
2021-07-09 13:12:02 +0300

    recepie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+  
+  const APP_ID = 'c99d7666'  // https://developer.edamam.com/admin/applications/1409621997281
+  const APP_KEY = 'ab32bd1e0d9ea5b9cc3f31ff8ea52331'  // https://developer.edamam.com/admin/applications/1409621997281
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <from className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit"></button>
+      </from>
     </div>
-  );
+  )
 }
 
 export default App;
